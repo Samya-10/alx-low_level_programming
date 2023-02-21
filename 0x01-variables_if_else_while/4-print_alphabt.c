87b9ff880510b814c74bfcac0f646fdb4156b441
Samya <Myemail@gmail.com>
2023-02-21 14:24:11 -0800

    print

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,18 +8,18 @@
 
 int main(void)
 {
-        int n = 97;
+	int n = 97;
 
 while (n <= 122)
 {
-	if(n == 101 || n == 113)
+	if (n == 101 || n == 113)
 	{
 		n++;
 		continue;
 	}
-		
-        putchar(n);
-        n++;
+
+	putchar(n);
+	n++;
 }
 
 putchar('\n');
