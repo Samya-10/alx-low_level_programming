d14c82594c972dd35d35e317b7761706b4e974cb
Samya <Myemail@gmail.com>
2023-02-22 15:47:52 -0800

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print the last digit
+ * @n: the integer to give its absolute value
+ * Return: void
+ */
+
+int print_last_digit(int n)
+{
+	int m;
+	m = n % 10;
+
+	if (m < 0)
+	{
+		m *= -1;
+	}
+
+	_putchar(m + '0');
+	return (m);
+}
