be8e01d38192210757ef5ffe78584f625600e363
Samya <Myemail@gmail.com>
2023-02-22 13:02:05 -0800

    alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print the alphabets
+ *
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+	}
+
+	_putchar('\n');
+}
