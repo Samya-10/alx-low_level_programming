3468f7946c39de6a3a6a2cb2a74262015a9eacc8
Samya <Myemail@gmail.com>
2023-02-22 09:57:05 -0800

    Third combinations

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+	int n, m, p;
+
+	for (n = '0'; n < '9'; n++)
+	{
+		for (m = n+1; m <= '9'; m++)
+		{
+			for (p = p + 1; p <= '9'; p++)
+			{
+				putchar(n);
+				putchar(m);
+				putchar(p);
+
+				if (m == '9')
+					break;
+
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+putchar('\n');
+return (0);
+}
