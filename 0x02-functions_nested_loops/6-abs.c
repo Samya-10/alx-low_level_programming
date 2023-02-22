a132dbee6a5263ce092c0626a0bfbd3ee411faf6
Samya <Myemail@gmail.com>
2023-02-22 15:03:41 -0800

    sign

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isalpha - print the alphabets
+ * @n: the integer to give its absolute value
+ * Return: void
+ */
+
+int _abs(int n)
+{
+
+	if (n >= 0)
+	{
+		return (n);
+	} else
+	{
+		return (-n);
+	}
+}
