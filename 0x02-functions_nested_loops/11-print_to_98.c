92c586ad7a563c3dad82fd5845a97697aa00c73a
Samya <Myemail@gmail.com>
2023-02-22 16:41:43 -0800

    98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * print_to_98 - print the sum of two integers
+ * @n: the integer
+ * Return: void
+ */
+
+void print_to_98(int n)
+{
+
+	if (n <= 98)
+	{
+		while (n <= 98)
+		{
+			_putchar(n);
+			if (n == 98)
+				break;
+			_putchar(',');
+			_putchar(' ');
+			n++;
+		}
+	} else
+	{
+		while (n >= 98)
+		{
+			_putchar(n);
+			if (n == 98)
+				break;
+			_putchar(',');
+			_putchar(' ');
+			n--;
+		}
+	}
+	_putchar('\n');
+}
