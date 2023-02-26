1137d54b6ea7ec3642cbf3b246cf69fccb3efdd9
Samya <Myemail@gmail.com>
2023-02-26 12:17:16 -0800

    print digits not 2 or 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,13 +8,13 @@
 
 void print_most_numbers(void)
 {
-        int n;
+	int n;
 
-        for (n = 0; n <= 9; n++)
-        {
+	for (n = 0; n <= 9; n++)
+	{
 		if (n == 2 || n == 4)
 			continue;
-                _putchar(n + '0');
-        }
-        _putchar('\n');
+		_putchar(n + '0');
+	}
+	_putchar('\n');
 }
