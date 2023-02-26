7fa0043a5ef6b279a1b8b71bfded24805ea4005b
Samya <Myemail@gmail.com>
2023-02-26 11:55:50 -0800

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* print_numbers - writes numbers from 0 to 9
+*
+* Return: void
+*/
+
+void print_numbers(void)
+{
+	int n;
+
+	n = 0;
+	while (n <= 9)
+	{
+		_putchar(n);
+		n++;
+	}
+	_putchar('\n');
+}
