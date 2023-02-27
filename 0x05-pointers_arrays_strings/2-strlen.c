e5885c9c239d31aa443f33419626d61bc6546206
Samya <Myemail@gmail.com>
2023-02-27 14:41:59 -0800

    length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - give the legth of the string
+ * @s: the string
+ * Return: the legth of the string
+ */
+
+int _strlen(char *s)
+{
+	int len;
+
+	len = strlen(s);
+	return(len);
+}
