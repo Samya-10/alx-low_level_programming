18932be773295644865babe42f67de34f1edd815
Samya <Myemail@gmail.com>
2023-02-27 11:27:08 -0800

    print number to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,36 +9,37 @@
 
 void print_to_98(int n)
 {
+	int i;
 
 	if (n <= 98)
 	{
-		for (;n <= 98; n++)
+		for (i = n; i <= 98; i++)
 		{
-			if (n == 98)
+			if (i == 98)
 			{
-				printf("%d", n);
+				printf("%d", i);
 				printf("\n");
 				break;
 			}
 			else
 			{
-				printf("%d, ", n);
+				printf("%d, ", i);
 			}
 		}
 	}
 	else
 	{
-		for (;n >= 98; n--)
+		for (i = n; i >= 98; i--)
 		{
-			if (n == 98)
+			if (i == 98)
 			{
-				printf("%d", n);
+				printf("%d", i);
 				printf("\n");
 				break;
 			}
 			else
 			{
-				printf("%d, ", n);
+				printf("%d, ", i);
 			}
 		}
 	}
