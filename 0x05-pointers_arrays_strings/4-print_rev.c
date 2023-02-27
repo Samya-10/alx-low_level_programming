0094c23a8a6555baa5c29b552629f0abeeee48de
Samya <Myemail@gmail.com>
2023-02-27 14:53:52 -0800

    printing a reversed string

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -12,10 +12,10 @@ void print_rev(char *s)
 {
 	int len, i;
 
-	len = strlen(s)
+	len = strlen(s);
 	for (i = len - 1; i >= 0; i--)
 	{
-		printf("%c", *(str + i));
+		printf("%c", *(s + i));
 	}
 
 	printf("\n");
