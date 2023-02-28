cc675e53e286c507d83929283df5cfe29ffad277
Samya <Myemail@gmail.com>
2023-02-28 13:04:00 -0800

    to integer

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * _atoi - string to integer
+ * @s: the string
+ * Return: the integer
+ */
+
+int _atoi(char *s)
+{
+	atoi(s);
+	return (s);
+}
