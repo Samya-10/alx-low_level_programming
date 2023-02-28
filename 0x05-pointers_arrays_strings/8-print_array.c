2f3457b5ce9cda60193c04eb4acf2b949b6ba7f7
Samya <Myemail@gmail.com>
2023-02-27 16:15:56 -0800

    print array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,7 +12,7 @@ void print_array(int *a, int n)
 {
 	int len, i;
 
-	len = sizeof(a) / sizeof(a[0]);
+	len = (int)(sizeof(a) / sizeof(a[0]));
 	for (i = 0; i < n && i < len ; i++)  
 	{
 		if (i == n - 1 || i == len -1)
