0a234b7a078dd6ba241e9a0f20b60a46d25f8401
Samya <Myemail@gmail.com>
2023-02-28 14:35:58 -0800

    the sum

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+	int i = 1, sum = 0;
+
+	for (i = 1; i < 1024; i++)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+		{
+			sum += i;;
+		}
+	}
+
+	printf("%d\n", sum);
+	return (0);
+}
