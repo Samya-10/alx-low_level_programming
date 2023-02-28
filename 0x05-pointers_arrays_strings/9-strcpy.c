7df999fbbd15ee31bcae2c034bbc740fecea0f85
Samya <Myemail@gmail.com>
2023-02-28 12:55:49 -0800

    pointers

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strcpy - print characters
+ * @dest: the string
+ * @src: the string
+ * Return: a pointer
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	strcpy(dest, src);
+	return (*dest);
+}
