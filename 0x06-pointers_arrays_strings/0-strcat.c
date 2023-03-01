163e662d361b9a9f09d260a9e216d455201d26d6
Samya <Myemail@gmail.com>
2023-03-01 13:16:23 -0800

    concatenate

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* _strcat - concatenate strings
+* @dest: the string 1
+* @src: the string 2
+* Return: concatenation
+*/
+
+char *_strcat(char *dest, char *src)
+{
+	strcat(dest, src);
+
+	retrn (dest);
+}
