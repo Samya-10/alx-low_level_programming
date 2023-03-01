3bc87f27864b7763ebf425b0391e98b7fee3fa7c
Samya <Myemail@gmail.com>
2023-03-01 12:07:42 -0800

    fibonacci_2

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+        int i;
+        long fibo, fibo_0 = 1, fibo_1 = 2, sum = 0;
+
+        while (fibo_0 <= 4000000)
+        {
+		if (fibo_0 % 2 == 0)
+                {
+			sum += fibo_0;
+                }
+
+
+                fibo = fibo_1 + fibo_0;
+                fibo_0 = fibo_1;
+                fibo_1 = fibo;
+        }
+	printf("%ld", sum);
+        return (0);
+}
