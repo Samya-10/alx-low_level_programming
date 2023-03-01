8035d18857d8a14cd06c73f23971fcbfc6075ffb
Samya <Myemail@gmail.com>
2023-03-01 11:39:11 -0800

    fibonacci_1

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -11,20 +11,20 @@
 int main(void)
 {
 
-	int i, fibo, fibo_0 = 0, fibo_1 = 1;
+	int i;
+	long fibo, fibo_0 = 0, fibo_1 = 1;
 
 	for (i = 2; i <= 50; i++)
 	{
-		printf("%d, ", fibo_0);
 
 		fibo = fibo_1 + fibo_0;
 		fibo_0 = fibo_1;
-		fibo_1 = fibo;		
+		fibo_1 = fibo;
 
-
+		printf("%ld, ", fibo_0);
 		if (i == 50)
 		{
-			printf("%d\n", fibo);
+			printf("%ld\n", fibo);
 		}
 	}
 	return (0);
