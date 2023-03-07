8fea00b80711066231f680cad3434c254fd90cb3
Samya <Myemail@gmail.com>
2023-03-07 10:44:32 -0800

    substring

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+* _strstr - the assemblance with a string
+* @haystack: the string to check
+* @needle: to string to compare
+* Return: the new string
+*/
+
+char *_strstr(char *haystack, char *needle)
+{
+
+	while (*haystack)
+	{
+		char *h = haystack;
+		char *n = needle;
+
+		while (*h == *n && *n != '\0')
+		{
+			h++;
+			n++;
+			if (*n == '\0')
+			{
+				return (haystack);		
+			}
+		}
+		haystack++;
+	}
+
+	return (NULL);
+}
