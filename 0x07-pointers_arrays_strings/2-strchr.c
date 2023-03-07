2fb1787186191d5a8089d02ff6726ceec7723702
Samya <Myemail@gmail.com>
2023-03-07 04:25:25 -0800

    memcopy

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+* _strchr - fill string with copy of a string
+* @s: the string to copy
+* @c: the number of characters to fill in *dest
+* Return: the new string
+*/
+
+char *_strchr(char *s, char c)
+{
+	int i;
+	char *f;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			f = s + i;		
+		}
+
+	}
+
+	return (f);
+}
